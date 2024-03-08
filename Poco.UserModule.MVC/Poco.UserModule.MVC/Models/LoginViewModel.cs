23dc83d651f273ba9ee676c992186306266e93be
kyawzinhtet-codigo <kyawzinhtet@codigo.co>
2024-03-08 08:47:12 +0700

    added login page

---
+++ Poco.UserModule.MVC/Poco.UserModule.MVC/Models/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Poco.UserModule.MVC.Models
+{
+	public class LoginViewModel
+	{
+		public string Email { set; get; }
+		public string Password { set; get; }
+		public Boolean IsRememberMe { set; get; } = false;
+	}
+}
